92e9f6bbfe532fa018074966ca812d397232ae60
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2023-05-24 17:22:56 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,33 @@
-(function ($) {
-	'use strict';
-	let windowWidth = $(window).width();
+'use strict';
+export let windowWidth = $(window).width();
 
-	$(function () {
-	});
-})(jQuery);
+export function handleSliderHero() {
+	const heroSlider = document.getElementById('hero-slider')
+	if (heroSlider) {
+		new Swiper('#hero-slider .swiper', {
+			speed: 1000,
+			slidesPerView: 1,
+			preloadImages: false,
+			effect: 'slide',
+			loop: true,
+			autoplay: {
+				delay: 8000,
+				disableOnInteraction: false,
+			},
+			navigation: {
+				nextEl: "#hero-slider .slider-button_next",
+				prevEl: "#hero-slider .slider-button_prev",
+			},
+			pagination: {
+				el: "#hero-slider .slider-pagination",
+				clickable: 1
+			}
+		});
+	}
+}
+
+window.addEventListener('load', function () {
+	handleSliderHero()
+});
+
+
